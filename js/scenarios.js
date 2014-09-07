57a419409eee8839f86381bdba60476df0952fdb
Noah Heldman <nheldman@fairwaytech.com>
2014-09-07 15:50:25 -0700

    Clean up modal code and css, add emphasis to incorrect answers

--- js/scenarios.js
+++ js/scenarios.js
@@ -1,5 +1,5 @@
 jQuery(function() {
-    jQuery('.m2-s1-4', '.m2-s1-5', '.m2-s1-6', '.m2-s1-7').click(function() {
+    jQuery('.open-modal').click(function() {
         event.preventDefault();
         jQuery(this).modal({fadeDuration:100});
     });
