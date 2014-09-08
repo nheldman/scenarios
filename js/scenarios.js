3eb008ad9035b9b2823f694875b26e30191881b8
Noah Heldman <nheldman@fairwaytech.com>
2014-09-07 20:35:55 -0700

    Testing responsive dialogs

--- js/scenarios.js
+++ js/scenarios.js
@@ -1,6 +1,6 @@
 jQuery(function() {
     jQuery('.open-modal').click(function() {
         event.preventDefault();
-        jQuery('#s4-incorrect').modal({fadeDuration:100});
+        jQuery(this).modal({fadeDuration:100});
     });
 });
