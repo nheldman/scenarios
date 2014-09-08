11492e3b7f464d118ec02fac4e8cae7fdb1fe836
Noah Heldman <nheldman@fairwaytech.com>
2014-09-07 21:44:00 -0700

    Try touchend for goto slide code

--- js/scenarios.js
+++ js/scenarios.js
@@ -3,4 +3,10 @@ jQuery(function() {
         event.preventDefault();
         jQuery(this).modal({fadeDuration:100});
     });
+
+    jQuery('.goto').on('click touchend', function() {
+        event.preventDefault();
+        var slide = parseInt(jQuery(this).data('slide'));
+        Dz.setSlide(slide);
+    });
 });
