e01eded97aa88a24dc76ebec704d1649f3e74346
Noah Heldman <nheldman@fairwaytech.com>
2014-09-07 21:22:30 -0700

    Test touchend event

--- js/scenarios.js
+++ js/scenarios.js
@@ -1,6 +1,6 @@
-//jQuery(function() {
-//    jQuery('.open-modal').click(function() {
-//        event.preventDefault();
-//        jQuery(this).modal({fadeDuration:100});
-//    });
-//});
+jQuery(function() {
+    jQuery('.open-modal').on('click touchend', function() {
+        event.preventDefault();
+        jQuery(this).modal({fadeDuration:100});
+    });
+});
