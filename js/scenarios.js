ae46f3808a906f8be40c3831bca70c0cab598a34
Noah Heldman <nheldman@fairwaytech.com>
2014-09-16 21:36:54 -0700

    Initial slides for Scenario 3

--- js/scenarios.js
+++ js/scenarios.js
@@ -1,28 +1,32 @@
 jQuery(function() {
-    jQuery('.open-modal').on('click touchend', function() {
-        event.preventDefault();
-        var $this = jQuery(this);
-        $this.modal({fadeDuration:100,closeText:'OK'});
+    var $ = jQuery;
 
-        if ($this.hasClass('correct')) {
-            $this.parents('section').children('.button').removeClass('hide');
+    $('.open-modal').on('click touchend', function() {
+        event.preventDefault();
+        $(this).modal({fadeDuration:100,closeText:'OK'});
+
+        if ($(this).hasClass('correct')) {
+            $(this).parents('section').children('.button').removeClass('hide');
         }
     });
 
     /* Allow navigating directly to a slide using: <a class="goto" data-slide="4">Go to Slide 4</a> */
-    jQuery('.goto').on('click touchend', function() {
+    $('.goto').on('click touchend', function() {
         event.preventDefault();
-        var slide = parseInt(jQuery(this).data('slide'));
+        var slide = parseInt($(this).data('slide'));
         Dz.setSlide(slide);
     });
 
     /* Dynamically scale the font size of modal p tags based on the number of letters in the modal text */
-    jQuery('.modal p').each(function () {
-        var $this = jQuery(this),
-            ln = $this.html().length,
+    $('.modal p').each(function () {
+        var ln = $(this).html().length,
             startSize = 30,
             size = startSize - (ln/100*2);
 
-        $this.css('font-size', Math.round(size))
+        $(this).css('font-size', Math.round(size))
+    });
+
+    $('#s6-signature-info').on('click touchend', function() {
+        $('#s6-signature-info-modal').modal({fadeDuration:100,closeText:'OK'});
     });
 });
